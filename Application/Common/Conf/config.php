bab864c2533194dcf91f2873baa1805aff15c04e
iamk <362934348@qq.com>
2022-07-13 10:42:06 +0800

    chore: modify database config;

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -32,7 +32,7 @@ return array (
 	'DB_TYPE' => 'mysql', // 数据库类型
 	
 	'DB_HOST' => 'mysql', // 服务器地址
-	'DB_NAME' => 'mbti', // 数据库名
+	'DB_NAME' => 'webnet', // 数据库名
 	'DB_USER' => 'root', // 用户名
 	'DB_PWD' => 'XyZ321!', // 密码
 	
